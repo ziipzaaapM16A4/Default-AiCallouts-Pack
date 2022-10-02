6e624430f12009a95fc836e79ea28d62f524c5db
Amir <Amir.boudani@gmail.com>
2022-10-02 02:30:36 +0200

    [fix] EC: Bad timing breaking AiC

--- EmergencyCall/EmergencyCall.cs
+++ EmergencyCall/EmergencyCall.cs
@@ -30,16 +30,8 @@ namespace EmergencyCall
                 while (!posFound)
                 {
                     roadside = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(AmbientAICallouts.API.Functions.minimumAiCalloutDistance + 10f, AmbientAICallouts.API.Functions.maximumAiCalloutDistance - 10f));
-                    //Vector3 irrelevant;
-                    //float heading = 0f;       //vieleicht guckt der MVA dann in fahrtrichtung der unit
-
-
-                    //NativeFunction.Natives.xA0F8A7517A273C05<bool>(roadside.X, roadside.Y, roadside.Z, heading, out roadside); //_GET_ROAD_SIDE_POINT_WITH_HEADING
-                    //NativeFunction.Natives.xFF071FB798B803B0<bool>(roadside.X, roadside.Y, roadside.Z, out irrelevant, out heading, 0, 3.0f, 0f); //GET_CLOSEST_VEHICLE_NODE_WITH_HEADING //Find Side of the road.
-
                     NativeFunction.Natives.xB61C8E878A4199CA<bool>(roadside, demandPavement, out roadside, 16); //GET_SAFE_COORD_FOR_PED
                     Location = roadside;
-
 
                     if (Functions.IsLocationAcceptedBySystem(Location))
                         posFound = true;
@@ -83,12 +75,12 @@ namespace EmergencyCall
                     GameFiber.Sleep(2000);
 
                     NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[0], "CODE_HUMAN_MEDIC_TIME_OF_DEATH", 40000, true);  //TASK_START_SCENARIO_IN_PLACE
-                    NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[1], "CODE_HUMAN_POLICE_INVESTIGATE", 40000, true);  //TASK_START_SCENARIO_IN_PLACE
+                    if (Units[0].UnitOfficers.Count > 1)  NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[1], "CODE_HUMAN_POLICE_INVESTIGATE", 40000, true);  //TASK_START_SCENARIO_IN_PLACE
                     var callerAnimation = caller.Tasks.PlayAnimation(new AnimationDictionary("oddjobs@towingangryidle_a"), "idle_c", 2f, AnimationFlags.Loop);
 
+                    GameFiber.SleepWhile(() => Units[0].UnitOfficers.Any( ofc => Helper.IsTaskActive(ofc, 15)), 1000);
                     GameFiber.SleepWhile(() => Units[0].UnitOfficers.Any( ofc => Helper.IsTaskActive(ofc, 118)), 60000);
                     caller.Tasks.Clear();
-                    //caller.Tasks.Wander();
                     caller.Dismiss();
 
                     if (Units[0].UnitOfficers.Count > 1)
@@ -101,7 +93,6 @@ namespace EmergencyCall
                         Rage.Native.NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[1], "WORLD_HUMAN_HANG_OUT_STREET", 10000, true); //TASK_START_SCENARIO_IN_PLACE
                         GameFiber.Sleep(10000);
                     }
-
                 }
                 return true;
             }
@@ -110,6 +101,7 @@ namespace EmergencyCall
             {
                 if (caller) caller.Delete();
                 LogTrivial_withAiC("ERROR: in AICallout object: At Process(): " + e);
+                AbortCode();
                 return false;
             }
         }
