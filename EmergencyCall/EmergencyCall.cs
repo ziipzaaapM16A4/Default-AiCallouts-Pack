2ea174d44eda045370fd077b029a3144242cd94f
Amir <Amir.boudani@gmail.com>
2023-07-22 18:39:00 +0200

    [rework] EC

--- EmergencyCall/EmergencyCall.cs
+++ EmergencyCall/EmergencyCall.cs
@@ -5,15 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using Rage;
 using Rage.Native;
+using LSPDFR_Functions = LSPD_First_Response.Mod.API.Functions;
+using Functions = AmbientAICallouts.API.Functions;
 using AmbientAICallouts.API;
+using LSPD_First_Response.Mod.API;
+using System.ComponentModel;
 
 namespace EmergencyCall
 {
     public class EmergencyCall : AiCallout
     {
         private Ped caller;
+        private bool warrantForArrest;
         private Rage.Object notepad = null;
         private Random rand = new Random();
+        private enum Estate { driving, parking, approaching, investigation, handling, pursuit};
+        
         public override bool Setup()
         {
             try
@@ -42,6 +49,7 @@ namespace EmergencyCall
                 }
 
                 caller = new Ped(Location);
+                warrantForArrest = LSPDFR_Functions.GetPersonaForPed(caller).Wanted;
                 Rage.Native.NativeFunction.Natives.x240A18690AE96513<bool>(caller.Position, out streetDirection, 0, 3f, 0f); //GET_CLOSEST_VEHICLE_NODE
                 Helper.TurnPedToFace(caller, streetDirection);
                 GameFiber.Sleep(2500);
@@ -60,41 +68,130 @@ namespace EmergencyCall
         {
             try
             {
-                if (!IsUnitInTime(Units[0], 100f, ResponseType == EResponseType.Code3 ? 130 : 260))  //if vehicle is never reaching its location                                                          //loger so that player can react
+                bool callactive = true;
+                uint timeStamp = Game.GameTime;
+                Estate status = Estate.driving;
+                int statusChild = 0;
+                int statusChild2 = 0;
+
+                LHandle pursuit;
+                bool pursuitWasSelfInitiated = false;
+
+                while (callactive)
                 {
-                    Disregard();
-                }
-                else
-                {
-                    OfficersArrive();
-                    Helper.PedsAproachAndFaceUntilReached(Units[0].UnitOfficers, caller.Position, 1f, 4f);
 
-
-                    foreach (var ofc in Units[0].UnitOfficers) { Helper.TurnPedToFace(ofc, caller); }
-                    Helper.TurnPedToFace(caller, Units[0].UnitOfficers[0]);
-                    GameFiber.Sleep(2000);
-
-                    NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[0], "CODE_HUMAN_MEDIC_TIME_OF_DEATH", 40000, true);  //TASK_START_SCENARIO_IN_PLACE
-                    if (Units[0].UnitOfficers.Count > 1)  NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[1], "CODE_HUMAN_POLICE_INVESTIGATE", 40000, true);  //TASK_START_SCENARIO_IN_PLACE
-                    var callerAnimation = caller.Tasks.PlayAnimation(new AnimationDictionary("oddjobs@towingangryidle_a"), "idle_c", 2f, AnimationFlags.Loop);
-
-                    GameFiber.SleepWhile(() => Units[0].UnitOfficers.Any( ofc => Helper.IsTaskActive(ofc, 15)), 1000);
-                    GameFiber.SleepWhile(() => Units[0].UnitOfficers.Any( ofc => Helper.IsTaskActive(ofc, 118)), 60000);
-                    caller.Tasks.Clear();
-                    caller.Dismiss();
-
-                    if (Units[0].UnitOfficers.Count > 1)
+                    //Pursuit - Any suspect in pursuit? -->> hunt felon
+                    if ((pursuit = LSPDFR_Functions.GetActivePursuit()) != null && Suspects.Any(s => (s ? LSPDFR_Functions.IsPedInPursuit(s) : false)) && !pursuitWasSelfInitiated)
                     {
-                        GameFiber.Sleep(1800);
-                        Helper.TurnPedToFace(Units[0].UnitOfficers[0], Units[0].UnitOfficers[1]);
-                        Helper.TurnPedToFace(Units[0].UnitOfficers[1], Units[0].UnitOfficers[0]);
-                        GameFiber.Sleep(3000);
-                        Rage.Native.NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[0], "WORLD_HUMAN_HANG_OUT_STREET", 10000, true); //TASK_START_SCENARIO_IN_PLACE
-                        Rage.Native.NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[1], "WORLD_HUMAN_HANG_OUT_STREET", 10000, true); //TASK_START_SCENARIO_IN_PLACE
-                        GameFiber.Sleep(10000);
+                        status = Estate.pursuit;
+                    } 
+
+
+                    switch (status)
+                    {
+                        case Estate.pursuit:
+                            LogTrivial_withAiC("Investigation turned into a Chase. Starting Pursuit");
+                            Units[0].PoliceVehicle.TopSpeed = 45f;
+                            foreach (var cop in Units[0].UnitOfficers) { LSPDFR_Functions.AddCopToPursuit(pursuit, cop); }
+
+                            if (!LSPDFR_Functions.IsPursuitCalledIn(pursuit)
+                            && Suspects.Any(s => (s ? Units[0].UnitOfficers.Any(ofc => ofc.DistanceTo(s) < 70f) : false)))
+                                LSPDFR_Functions.SetPursuitAsCalledIn(pursuit);
+
+                            //aic.OnScene = true; //Missing OnScene detail
+                            Units[0].UnitStatus = EUnitStatus.OnScene;
+                            GameFiber.SleepWhile(() => LSPDFR_Functions.IsPursuitStillRunning(pursuit), 360000); //LSPDFR Pursuit managing now
+                            callactive = false;
+                            break;
+                        case Estate.driving:
+                            if (Units[0].PoliceVehicle.Position.DistanceTo(Location) < 70f) { timeStamp = Game.GameTime; status = Estate.parking; }
+                            else if (timeStamp + (ResponseType == EResponseType.Code3 ? 130 : 260) * 1000 < Game.GameTime) { callactive = false; Disregard(); }//if vehicle is never reaching its location.
+                            break;
+                        case Estate.parking:
+                            OfficersArrive();
+                            status = Estate.approaching;
+                            break;
+                        case Estate.approaching:
+                            Helper.CopsApproachAndFaceUntilReachedButStopOnPursuitInDistance(Units[0].UnitOfficers, caller.Position, 1f, 4f, 60000);
+                            status = Estate.investigation;
+                            break;
+                        case Estate.investigation:
+                            switch (statusChild) {
+                                case 0: //Posisioning and looking at caller
+                                    foreach (var ofc in Units[0].UnitOfficers) { Helper.TurnPedToFace(ofc, caller); }
+                                    Helper.TurnPedToFace(caller, Units[0].UnitOfficers[0]);
+                                    GameFiber.Sleep(2000);
+                                    break;
+                                case 1: //Talking to caller
+                                    if (statusChild2 == 0)
+                                    {
+                                        NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[0], "CODE_HUMAN_MEDIC_TIME_OF_DEATH", 40000, true);  //TASK_START_SCENARIO_IN_PLACE
+                                        if (Units[0].UnitOfficers.Count > 1) NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[1], "CODE_HUMAN_POLICE_INVESTIGATE", 40000, true);  //TASK_START_SCENARIO_IN_PLACE
+                                        var callerAnimation = caller.Tasks.PlayAnimation(new AnimationDictionary("oddjobs@towingangryidle_a"), "idle_c", 2f, AnimationFlags.Loop);
+                                        timeStamp = Game.GameTime;
+                                        statusChild2 = 1;
+                                    }
+                                    else if (statusChild2 == 1 ? Units[0].UnitOfficers.Any(ofc => Helper.IsTaskActive(ofc, 15)) && timeStamp + 1000 < Game.GameTime : false)
+                                    {
+                                        timeStamp = Game.GameTime;
+                                    }
+                                    else if (statusChild2 == 2 ? Units[0].UnitOfficers.Any(ofc => Helper.IsTaskActive(ofc, 118)) && timeStamp + 60000 < Game.GameTime : false)
+                                    {
+                                        timeStamp = Game.GameTime;
+                                        statusChild2 = 3;
+                                    }
+                                    else if (statusChild2 == 3)
+                                    {
+                                        if (warrantForArrest) { status = Estate.handling; } else { statusChild = 2; statusChild2 = 0; }
+                                    }
+                                    break;
+                                case 2: //Releaseing caller
+                                    caller.Tasks.Clear();
+                                    caller.Dismiss();
+                                    if (Units[0].UnitOfficers.Count > 1 && rand.Next(1) == 1) { statusChild = 3; }
+                                    break;
+                                case 3:  //Talking to each other (sometimes)
+                                    if (statusChild2 == 0)
+                                    {
+                                        timeStamp = Game.GameTime;
+                                        statusChild2 = 1;
+                                    }
+                                    else if (statusChild2 == 1)
+                                    {
+                                        if (timeStamp + 1800 < Game.GameTime) { timeStamp = Game.GameTime; statusChild2 = 2; }
+                                    }
+                                    else if (statusChild2 == 2)
+                                    {
+                                        Helper.TurnPedToFace(Units[0].UnitOfficers[0], Units[0].UnitOfficers[1]);
+                                        Helper.TurnPedToFace(Units[0].UnitOfficers[1], Units[0].UnitOfficers[0]);
+                                        timeStamp = Game.GameTime;
+                                        statusChild2 = 3;
+                                    }
+                                    else if (statusChild2 == 3)
+                                    {
+                                        if (timeStamp + 3000 < Game.GameTime) { timeStamp = Game.GameTime; statusChild2 = 4; }
+                                    }
+                                    else if (statusChild2 == 4)
+                                    {
+                                        Rage.Native.NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[0], "WORLD_HUMAN_HANG_OUT_STREET", 10000, true); //TASK_START_SCENARIO_IN_PLACE
+                                        Rage.Native.NativeFunction.Natives.x142A02425FF02BD9(Units[0].UnitOfficers[1], "WORLD_HUMAN_HANG_OUT_STREET", 10000, true); //TASK_START_SCENARIO_IN_PLACE
+                                        timeStamp = Game.GameTime;
+                                        statusChild2 = 5;
+                                    }
+                                    else if (statusChild2 == 5) { if (timeStamp + 10000 < Game.GameTime) { timeStamp = Game.GameTime; callactive = false; } }
+                                    break;
+                                }
+                                break;
+                        case Estate.handling: //if caller is suspect, then arrest him
+                            var Arrest = LSPDFR_Functions.CreatePursuit();
+                            LSPDFR_Functions.SetPursuitInvestigativeMode(Arrest, true);
+                            foreach (var ofc in Units[0].UnitOfficers) LSPDFR_Functions.AddCopToPursuit(Arrest, ofc);
+                            LSPDFR_Functions.AddPedToPursuit(Arrest, caller);
+                            while (LSPDFR_Functions.IsPursuitStillRunning(Arrest)) { GameFiber.Sleep(500); } //ToDo: Bad Practice. Blocks the thread. Find a better way to do this.
+                            break;
                     }
+
                 }
-                return true;
             }
             catch (System.Threading.ThreadAbortException) { if (caller) caller.Delete(); return false; }
             catch (Exception e)
@@ -104,6 +201,7 @@ namespace EmergencyCall
                 AbortCode();
                 return false;
             }
+            return true;
         }
 
         private void OfficersArrive()
